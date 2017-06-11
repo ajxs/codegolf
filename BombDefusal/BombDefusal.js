70ae22d2f06b1c496748499393de39e41815591d
Anthony JXS <ajxscc@gmail.com>
2017-06-12 00:23:01 +1000

    Bomb defusal improvements

--- BombDefusal/BombDefusal.js
+++ BombDefusal/BombDefusal.js
@@ -1,7 +1,6 @@
-"use strict";
-let defuse = (_wires_, _odd_, c=(a,x)=>a.filter(i=>i==x).length) => [,,,
-	()=> !c(_wires_, 'R') ? 2 : _wires_[2] == 'W' ? 3 : c(_wires_, 'U') > 1 ? _wires_.lastIndexOf('U')+1 : 3,
-	()=> c(_wires_, 'R') > 1 && _odd_ ? _wires_.lastIndexOf('R')+1 : _wires_[3] == 'Y' && !c(_wires_, 'R') ? 1 : c(_wires_, 'U') == 1 ? 1 : c(_wires_, 'Y') > 1 ? 4 : 2,
-	()=> _wires_[4] == 'B' && _odd_ ? 4 : c(_wires_, 'R') == 1 && c(_wires_, 'Y') > 1 ? 1 : !c(_wires_, 'B') ? 2 : 1,
-	() => !c(_wires_, 'Y') && _odd_ ? 3 : c(_wires_, 'Y') == 1 && c(_wires_, 'W') > 1 ? 4 : !c(_wires_, 'R') ? 6 : 4
-][_wires_.length]();
+let defuse=(w,o,c=x=>w.filter(i=>i==x).length,q="lastIndexOf",r=c('R'),y=c('Y'),u=c('U'))=>[,,,
+	()=>!r?2:w[2]=='W'?3:u>1?w[q]('U')+1:3,
+	()=>r>1&&o?w[q]('R')+1:w[3]=='Y'&&!r?1:u==1?1:y>1?4:2,
+	()=>w[4]=='B'&&o?4:r==1&&y>1?1:!c('B')?2:1,
+	()=>!y&&o?3:y==1&&c('W')>1?4:!r?6:4
+][w.length]();
